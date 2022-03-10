915f9bd18c3d921d0a46f0277961136bae85e890
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-03-10 10:17:28 -0800

    add 07-filter challenges and tests

--- test/tests.js
+++ test/tests.js
@@ -4,7 +4,7 @@ import './02-by-val-by-ref.test.js';
 import './03-sort.test.js';
 import './04-regex-part-1.test.js';
 import './05-split-join-slice-splice.test.js';
-// import './06-map.test.js';
+import './06-map.test.js';
 // import './07-filter.test.js';
 // import './08-reduce.test.js';
 // import './09-object-iteration.test.js';
